c2ca47f0cfc0c3d8dacfa48ca770dd068bab4aa4
Daniel Cunningham <138261809+danielxcastle@users.noreply.github.com>
2023-08-16 22:53:53 +0000

    button added

--- src/app.js
+++ src/app.js
@@ -6,6 +6,45 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♦", "♥", "♠", "♣"];
+  const numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let button = document.querySelector("#button");
+  button.addEventListener("click", () => {
+    const chosenSuit = Math.floor(Math.random() * suits.length);
+    const chosenNumber = Math.floor(Math.random() * numbers.length);
+
+    let topSuit = document.querySelector("#suit-top");
+    let bottomSuit = document.querySelector("#suit-bottom");
+
+    topSuit.textContent = suits[chosenSuit];
+    bottomSuit.textContent = suits[chosenSuit];
+
+    let cardNumber = document.querySelector("#number");
+    cardNumber.textContent = numbers[chosenNumber];
+
+    topSuit.style.color =
+      topSuit.textContent === "♥" || topSuit.textContent === "♦"
+        ? "red"
+        : "black";
+
+    bottomSuit.style.color =
+      bottomSuit.textContent === "♥" || bottomSuit.textContent === "♦"
+        ? "red"
+        : "black";
+  });
 };
